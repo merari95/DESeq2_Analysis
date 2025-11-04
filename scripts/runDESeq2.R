0922d462a97891b925576bbbfba4212469e7e5a6
Merari Santana <santanamerari@gmail.com>
2025-11-03 19:41:11 -0800

    added data and DESseq2 scripts

---
+++ scripts/runDESeq2.R
@@ -0,0 +1,23 @@
+#script to perform differential gene expression analysis using DESeq2 package
+#setwd("~/Desktop/demo/DESeq2_tutorial/data")
+
+#load libraries
+library(DESeq2)
+library(tidyverse)
+library(airway)
+
+
+#Step 1: preparing count data --------
+
+#read in counts data
+counts_data <- read.csv('counts_data.csv')
+head(counts_data)
+
+#read in sample info
+colData <- read.csv('sample_info.csv')
+
+#making sure the row names in colData matches the column names in counts_data
+all(colnames(counts_data)%in% rownames(colData))
+#are they in the same order?
+
+all(colnames(counts_data) == rownames(colData))
